01100a1e39e68337ef459aca6ec5428d9fba009a
Xiao Shou <49129793+xshou1990@users.noreply.github.com>
2023-08-02 14:04:40 -0400

    Add files via upload

---
+++ transformer_baseline_prediction/diabetes_run.sh
@@ -0,0 +1,18 @@
+device=0
+data=../data/diabetes_data/
+batch=16
+n_head=4
+n_layers=4
+d_model=256
+d_inner=128
+d_k=128
+d_v=128
+dropout=0.1
+lr=1e-4
+epoch=300
+log=diabetes_log.txt
+
+
+
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$device python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -epoch $epoch -log $log
+
---
+++ transformer_baseline_prediction/so_run.sh
@@ -0,0 +1,17 @@
+device=0
+data=../data/so_data/
+batch=16
+n_head=4
+n_layers=4
+d_model=512
+d_inner=256
+d_k=256
+d_v=256
+dropout=0.1
+lr=1e-4
+epoch=300
+log=so_log.txt
+
+
+
+CUDA_DEVICE_ORDER=PCI_BUS_ID CUDA_VISIBLE_DEVICES=$device python Main.py -data $data -batch $batch -n_head $n_head -n_layers $n_layers -d_model $d_model -d_inner $d_inner -d_k $d_k -d_v $d_v -dropout $dropout -lr $lr -epoch $epoch -log $log
